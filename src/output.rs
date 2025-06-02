f60018a45b62db43e649003492b563236a0246bf
Matan Lurey <matan@lurey.org>
2025-06-01 23:14:22 -0700

    Add single test.

--- src/output.rs
+++ src/output.rs
@@ -1,7 +1,7 @@
 use core::fmt;
 use std::fmt::Debug;
 
-use proc_result::ProcResult;
+use proc_result::{ProcResult, raw::RawExitCode};
 
 /// The output of a finished process.
 #[derive(Clone, PartialEq, Eq)]
@@ -20,6 +20,12 @@ pub struct Output {
 impl Debug for Output {
     #[allow(clippy::ref_binding_to_reference)]
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let result_code: i64 = match self.result {
+            ProcResult::Unix(ref status) => status.to_raw().into(),
+            ProcResult::Windows(ref status) => status.to_raw().into(),
+            _ => -1,
+        };
+
         let stdout_utf8 = std::str::from_utf8(&self.stdout);
         let stdout_debug: &dyn fmt::Debug = match stdout_utf8 {
             Ok(ref s) => s,
@@ -32,9 +38,27 @@ impl Debug for Output {
         };
 
         f.debug_struct("Output")
-            .field("result", &self.result)
+            .field("result", &result_code)
             .field("stdout", stdout_debug)
             .field("stderr", stderr_debug)
             .finish()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn output_debug() {
+        let output = Output {
+            result: ProcResult::Unix(proc_result::unix::WaitStatus::from_raw(0)),
+            stdout: b"Hello, World!".to_vec(),
+            stderr: b"".to_vec(),
+        };
+        assert_eq!(
+            format!("{output:?}"),
+            "Output { result: 0, stdout: \"Hello, World!\", stderr: \"\" }"
+        );
     }
 }
